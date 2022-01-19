a113f60374e29bcf69d2b53989828e1c7a9a412e
ibrahim Burak Ezdemir <ibrahimezdemir@gmail.com>
2022-01-19 11:26:30 +0300

    Initial Commit

---
+++ LandmarkBook/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LandmarkBook
+//
+//  Created by Burak on 19.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
